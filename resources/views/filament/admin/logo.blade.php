0a47e40020c771a8b889107aa6d6988b020ca866
DeKils <audiadyan123@gmail.com>
2025-01-23 11:31:59 +0800

    Update logo.blade.php

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex gap-2">
+<div class="flex gap-2 justify-center">
     <img src="{{ asset('images/logo.webp') }}" alt="">
     <p class="text-xl font-medium">{{ env('APP_NAME', 'Balita') }}</p>
 </div>
